0ef3d02cb408a7a92cc8b1380be47b09069b6798
saurabh <saurabh.digambar@arxada.com>
2023-10-19 11:44:21 +0530

    branch-deploy

--- .github/scripts/branch-deploy.sh
+++ .github/scripts/branch-deploy.sh
@@ -171,6 +171,15 @@ file="./$t_env/applications/$t_app/kustomization.yaml"
 sed -i '/# lock target environment starts/,/# lock target environment ends/d' "${file}"
 done
 }
+
+gh-cli() {
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+}
 #===============================================================================================
 # MAIN
 
@@ -198,6 +207,9 @@ case $ACTION in
    ;;
  cleanup)
    cleanup
+   ;;
+gh-cli)
+   gh-cli
    ;;
  *)
    exit_with_error
